8c98284663ffee722fe5641bae1ce983ee951009
bjastrzebs002 <bartek.jastrzebski0010@gmail.com>
2019-06-06 14:30:16 +0200

    delete project's copy

--- language_recognizing/language_recognizing/FileReader.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  FileReader.cpp
-//  language_recognizing
-//
-//  Created by Bartosz Jastrzębski on 06/04/2019.
-//  Copyright © 2019 Bartosz Jastrzębski. All rights reserved.
-//
-
-#include "FileReader.hpp"
