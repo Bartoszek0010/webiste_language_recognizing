1628a0b0fd11b6f76d1916d3ced2f61d4d509da3
bjastrzebs002 <bartek.jastrzebski0010@gmail.com>
2019-06-07 01:37:33 +0200

    Add unique vector of unigrams

--- code/all_bigrams.py
+++ code/all_bigrams.py
@@ -1,10 +1,16 @@
 unique_list = list('qwertyuiopasdfghjklzxcvbnm,. ?!ęóąśżźćńłàèéìíòùñú')
-bigrams = {}
 unique_list.sort()
 str_to_save = ''
 for i in unique_list:
     for j in unique_list:
         str_to_save += i + j +'\n'
-        print(i + j)
 file = open("bigrams_dictionary.txt", "w")
 file.write(str_to_save)
+
+unique_list_unigram = list('qwertyuiopasdfghjklzxcvbnmęóąśżźćńłàèéìíòùñú')
+unique_list_unigram.sort()
+str_to_save = ''
+for i in unique_list_unigram:
+    str_to_save += i + '\n'
+file_unigrams = open("unigrams_dictionary.txt", "w")
+file_unigrams.write(str_to_save)
